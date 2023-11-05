7657c3464801f2a07d71f75dffa98cef388b3d1b
Meryem Genc <67630134+MeryemGenc@users.noreply.github.com>
2023-11-05 14:20:42 +0300

    map_check1

--- map_utils/map_check.c
+++ map_utils/map_check.c
@@ -21,7 +21,7 @@ int map_check(t_map *map)
                 return (print_err("2Map must be surround by walls."));
             if (map->map[i][j] == ' ' && (map->map[i][j+1] != '1' || map->map[i][j-1] != '1'))// duvar kapanmadan space varsa
             {
-                printf("\ny:%s x:%s\n", j,i);
+                printf("\ny:%d x:%d\n", j,i);
                 return (print_err("2Map must be surround by walls."));
             }
         }
